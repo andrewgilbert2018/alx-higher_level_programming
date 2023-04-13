92cb17375ac7bc9736830fd488aac77d4c15d610
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-04-12 17:03:44 -0700

    Task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,5 +14,5 @@ def save_to_json_file(my_obj, filename):
     Raises:
         Exception: when the object can't be encoded
     '''
-    with open(filename, 'r' encoding="utf-8") as f:
+    with open(filename, 'w' encoding="utf-8") as f:
         json.dump(my_obj, f)
