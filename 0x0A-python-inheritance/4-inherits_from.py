900bca7635b96172a4e190f4fc6eed4d4dc7b1eb
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-04-12 18:05:55 -0700

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-function that returns True if the object is an instance of a class that inherited from the specified class ; otherwise False
+A function that returns True if the object is an instance
 '''
 
 
