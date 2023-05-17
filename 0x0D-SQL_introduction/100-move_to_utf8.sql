b20361cbb5a3d5f15a7ae219d5ac1194d9ad2e8d
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-05-17 05:57:17 -0700

    move to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- change database, name, table to utf-8
+USE hbtn_0c_0;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
