f617e9d1b408736b461ef4ccb7adac656dcf7bc4
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-05-25 23:58:59 -0700

    no genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,2 @@
+-- a script that import database dump from hbtn_0d_tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.genre_id is NULL ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
