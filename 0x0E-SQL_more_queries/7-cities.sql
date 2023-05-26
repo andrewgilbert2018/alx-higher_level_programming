c5a76f63f6f6038bc9864aa2c1fcd04894cc20e8
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-05-25 23:36:10 -0700

    cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- a script that creat a database hbtn_0d_us and table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY(state_id) REFERENCES states(id) )
