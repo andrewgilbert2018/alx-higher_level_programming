7a5020ee3a8480becb4af79afc64a3e8314df1d0
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-05-26 00:02:08 -0700

    count shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- a script that import from a database
+SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_show_genres.genre_id ORDER BY number_of_shows DESC;
