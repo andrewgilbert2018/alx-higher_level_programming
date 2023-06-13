20fac3942f305a46e651b943c1d5e4650093ad68
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-13 15:48:24 -0700

    2 arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// a script that print a message depending of the number of arguments passed
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
