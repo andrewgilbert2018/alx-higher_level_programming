9b7593a66f170a7b21c68d15a6221d2a87c07b68
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-14 10:11:01 -0700

    101 sorted

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence
+const dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
+}
+console.log(newDict);
