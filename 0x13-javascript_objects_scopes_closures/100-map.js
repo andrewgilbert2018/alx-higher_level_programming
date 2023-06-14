ec690017f4930b22ab493cce6aba96c7cef7499a
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-14 10:26:51 -0700

    100 map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 // a script that imports an array and computes a new array.
-exports.list = [1, 2, 3, 4, 5];
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
