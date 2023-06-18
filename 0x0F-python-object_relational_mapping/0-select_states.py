69351125275d828bbc34b1a2698cf9e090f4c2b9
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-18 05:08:31 -0700

    0 select states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a python script that help to select states from a server """
+import MySQLdb
+from sys import argv
+
+
+def select_states():
+    """function to access database and  print states """
+    database = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3]
+    )
+    current = database.cursor()
+    current.execute("SELECT * FROM states ORDER BY id")
+    rows = current.fetchall()
+    for row in rows:
+        print(row)
+    current.close()
+    database.close()
+
+if __name__ == "__main__":
+    select_states()
