04892370a5a79e76c9da84b94bf6421592a9901f
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-18 14:57:04 -0700

    1 filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""python script to filter states """
+import MySQLdb
+from sys import argv
+
+
+def select_states():
+    """ access database and  print states that  start with N """
+    database = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3]
+    )
+    current = database.cursor()
+    curent.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    rows = curent.fetchall()
+    for row in rows:
+        print(row)
+    current.close()
+    database.close()
+
+if __name__ == "__main__":
+    select_states()
