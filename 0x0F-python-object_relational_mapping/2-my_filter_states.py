b3082154234e5bfcb587e48461bce2fa87fd5121
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-18 15:41:31 -0700

    2 my filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" a python script that filter states with sql inj guard"""
+import MySQLdb
+from sys import argv
+
+
+def select_states():
+    """a functon that  access database print states by input """
+    database = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3]
+    )
+    cut = database.cursor()
+    sql_string = "SELECT * FROM states WHERE name \
+        LIKE BINARY %s"
+    cut.execute(sql_string, (argv[4],))
+    rows = cut.fetchall()
+    for row in rows:
+        print(row)
+    cut.close()
+    database.close()
+
+if __name__ == "__main__":
+    select_states(
