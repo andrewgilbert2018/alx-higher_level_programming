362a6e01c04565f16586c0ee3e702bd76dce0957
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-19 04:35:08 -0700

    9 model state filter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" a python script that fetch all states with a """
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def fetch_states():
+    """ a function that access database and print states """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    for state in session.query(State).filter(State.name.like('%a%')):
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
+
+if __name__ == "__main__":
+    fetch_states()
