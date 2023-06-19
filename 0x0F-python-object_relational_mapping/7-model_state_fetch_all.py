a20fb26280313a92f0959a709407e4fb99a35fa6
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-19 04:19:56 -0700

    7 model state fetch all sql

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" a python script that fetch all states """
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def fetch_states():
+    """ a function that access a database and  print states """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
+
+if __name__ == "__main__":
+    fetch_states()
