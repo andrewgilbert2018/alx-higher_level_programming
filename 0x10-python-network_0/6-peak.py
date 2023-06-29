055af608e338ab19480942e38e331e21ec2a919c
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-29 12:53:44 -0700

    6 peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,5 +14,5 @@ def find_peak(list_of_integers):
         elif(list_of_integers[1] > list_of_integers[2]):
             return list_of_integers[1]
         else:
-            return(max(list_of_integers))
+            return(max if list_of_integers >0(list_of_integers))
     return(None)
