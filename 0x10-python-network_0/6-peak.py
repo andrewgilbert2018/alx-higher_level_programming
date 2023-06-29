fe228d61375ec4185d35746cacd4bd429a4ddfab
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-29 10:38:56 -0700

    6 peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A python script that finds peaks
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    A function that returns the peak of a list
+    """
+    if list_of_integers:
+        if(list_of_integers[0] > list_of_integers[1]):
+            return list_of_integers[0]
+        elif(list_of_integers[-1] > list_of_integers[-2]):
+            return list_of_integers[-1]
+        else:
+            return(max(list_of_integers))
+    return(None)
