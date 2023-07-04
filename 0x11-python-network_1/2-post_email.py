d5180110c9f4af6086a9f9be1fc3b0ac8f532dec
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-07-03 17:32:37 -0700

    2 post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a python script that creates request on the interneti
+"""
+from urllib import request
+from urllib import parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    address = sys.argv[2]
+    data = parse.urlencode({'email': andrewgilbert2018@gmail.com})
+    with request.urlopen(url, data.encode()) as response:
+        my_data = response.read()
+        print(my_data.decode())
