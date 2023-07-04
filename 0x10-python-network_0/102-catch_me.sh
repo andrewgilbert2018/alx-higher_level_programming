afb527b2dceffe48a836b5c4551e6a85b96f5979
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-07-03 18:12:58 -0700

    102 catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a bash script that make a request to 0.0.0.0:5000/catch_me
- curl -sL --request PUT 0.0.0.0:5000/catch_me --data "user_id=98" -H "Origin:You find me!"
+curl -sL --request PUT 0.0.0.0:5000/catch_me --data "user_id=98" -H "Origin: You got me!"
