e17b185dd3b1fd5460a810d366b3d75dce3ad56e
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-07-03 17:34:30 -0700

    2 post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     address = sys.argv[2]
-    data = parse.urlencode({'email': andrewgilbert2018@gmail.com})
+    data = parse.urlencode({'email': address})
     with request.urlopen(url, data.encode()) as response:
         my_data = response.read()
         print(my_data.decode())
