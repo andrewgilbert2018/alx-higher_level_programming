e7f274ab64a547dd955f171c56dbb98202dffc5a
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-08-02 05:42:24 -0700

    Task 11

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const $ = window.$;
+$(document).ready(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list').children().last().remove();
+  });
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
